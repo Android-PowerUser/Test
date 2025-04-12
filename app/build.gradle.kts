ea757b9bc3de548be08dce2795d2de6f3088dd28
Android PowerUser <88908510+Android-PowerUser@users.noreply.github.com>
2025-04-12 10:20:40 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,14 +63,15 @@ android {
 }
 
 dependencies {
-    // Required for Android Accessibility Service
-    implementation("androidx.core:core:1.9.0")
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+
+    // Required for Android Accessibility Service
+    implementation("androidx.core:core:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 
     // Required for one-shot operations (to use `ListenableFuture` from Guava Android)
     implementation("com.google.guava:guava:31.0.1-android")
