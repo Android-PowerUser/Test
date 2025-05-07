d7b9acfd7ec66852e6a6bbc4b480252aaa434a66
Android PowerUser <88908510+Android-PowerUser@users.noreply.github.com>
2025-05-08 00:57:18 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,9 @@ dependencies {
 
     implementation("io.coil-kt:coil-compose:2.5.0")
 
+    // Google Play Billing Library
+    implementation("com.android.billingclient:billing-ktx:7.1.1") // Latest version as per documentation
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -99,3 +102,4 @@ dependencies {
 
     implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
 }
+
